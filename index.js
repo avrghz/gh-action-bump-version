806617fcd331df03b85b6191dd724b29554d3e47
AswinTom Varghese <aswin.thunchathu@gmail.com>
2020-07-05 09:02:21 +0200

    6

--- index.js
+++ index.js
@@ -1,29 +1,7 @@
 const { Toolkit } = require("actions-toolkit");
 const { execSync } = require("child_process");
 
-// Change working directory if user defined PACKAGEJSON_DIR
-// if (process.env.PACKAGEJSON_DIR) {
-//   process.env.GITHUB_WORKSPACE = `${process.env.GITHUB_WORKSPACE}/${process.env.PACKAGEJSON_DIR}`;
-//   process.chdir(process.env.GITHUB_WORKSPACE);
-// }
-
-console.log("PACKAGEJSON_DIR 1", process.env["INPUT_SUB-PACKAGE"]);
-
-// Run your GitHub Action!
-Toolkit.run(async (tools) => {
-  console.log("PACKAGEJSON_DIR 2", process.env["INPUT_SUB-PACKAGE"]);
-  const pkg = tools.getPackageJSON();
-  const event = tools.context.payload;
-
-  const messages = event.commits.map((commit) => commit.message + "\n" + commit.body);
-
-  const commitMessage = "version bump to";
-  const isVersionBump = messages.map((message) => message.toLowerCase().includes(commitMessage)).includes(true);
-  if (isVersionBump) {
-    tools.exit.success("No action necessary!");
-    return;
-  }
-
+const getVersion = (messages) => {
   let version = "patch";
   if (messages.map((message) => message.includes("BREAKING CHANGE") || message.includes("major")).includes(true)) {
     version = "major";
@@ -35,15 +13,52 @@ Toolkit.run(async (tools) => {
     version = "minor";
   }
 
+  return version;
+};
+
+const checkIsVersionBump = (messages, commitMessage) =>
+  messages.map((message) => message.toLowerCase().includes(commitMessage)).includes(true);
+
+const getCommitMessages = (commits) => commits.map((commit) => commit.message + "\n" + commit.body);
+
+const setGitConfig = async (tools) => {
+  await tools.runInWorkspace("git", [
+    "config",
+    "user.name",
+    `"${process.env.GITHUB_USER || "Automated Version Bump"}"`,
+  ]);
+  await tools.runInWorkspace("git", ["config", "user.email", process.env.GITHUB_EMAIL]);
+};
+
+// Change working directory if user defined PACKAGEJSON_DIR
+// if (process.env.PACKAGEJSON_DIR) {
+//   process.env.GITHUB_WORKSPACE = `${process.env.GITHUB_WORKSPACE}/${process.env.PACKAGEJSON_DIR}`;
+//   process.chdir(process.env.GITHUB_WORKSPACE);
+// }
+
+// Run your GitHub Action!
+Toolkit.run(async (tools) => {
+  const commitMessage = "version bump to";
+
+  const pkg = tools.getPackageJSON();
+  const event = tools.context.payload;
+
+  const messages = getCommitMessages(event.commits);
+
+  const isVersionBump = checkIsVersionBump(messages, commitMessage);
+
+  if (isVersionBump) {
+    tools.exit.success("No action necessary!");
+    return;
+  }
+
+  const version = getVersion(messages);
+
   try {
     const current = pkg.version.toString();
+
     // set git user
-    await tools.runInWorkspace("git", [
-      "config",
-      "user.name",
-      `"${process.env.GITHUB_USER || "Automated Version Bump"}"`,
-    ]);
-    await tools.runInWorkspace("git", ["config", "user.email", process.env.GITHUB_EMAIL]);
+    await setGitConfig(tools);
 
     const currentBranch = /refs\/[a-zA-Z]+\/(.*)/.exec(process.env.GITHUB_REF)[1];
     console.log("currentBranch:", currentBranch);
@@ -56,21 +71,26 @@ Toolkit.run(async (tools) => {
     await tools.runInWorkspace("git", ["commit", "-a", "-m", `ci: ${commitMessage} ${newVersion}`]);
 
     // now go to the actual branch to perform the same versioning
-    // await tools.runInWorkspace("git", ["checkout", currentBranch]);
-    // await tools.runInWorkspace("npm", ["version", "--allow-same-version=true", "--git-tag-version=false", current]);
-    // console.log("current:", current, "/", "version:", version);
-    // newVersion = execSync(`npm version --git-tag-version=false ${version}`).toString().trim();
-    // newVersion = `${process.env["INPUT_TAG-PREFIX"]}${newVersion}`;
-    // console.log("new version:", newVersion);
+    await tools.runInWorkspace("git", ["checkout", currentBranch]);
+    await tools.runInWorkspace("npm", ["version", "--allow-same-version=true", "--git-tag-version=false", current]);
+    console.log("current:", current, "/", "version:", version);
+    newVersion = execSync(`npm version --git-tag-version=false ${version}`).toString().trim();
+    newVersion = `${process.env["INPUT_TAG-PREFIX"]}${newVersion}`;
 
-    console.log("PACKAGEJSON_DIR 3", process.env["INPUT_SUB-PACKAGE"], process.env["INPUT_TAG-PREFIX"]);
+    if (process.env["INPUT_SUB-PACKAGE"]) {
+      console.log("Change sub package started");
+      tools.runInWorkspace("cd", [process.env["INPUT_SUB-PACKAGE"]]);
+      execSync(`npm version --git-tag-version=false ${version}`).toString().trim();
+      tools.runInWorkspace("cd", [".."]);
+    }
 
+    console.log("new version:", newVersion);
     const remoteRepo = `https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`;
     // console.log(Buffer.from(remoteRepo).toString('base64'))
     // await tools.runInWorkspace("git", ["tag", newVersion]);
     // await tools.runInWorkspace("git", ["push", remoteRepo, "--follow-tags"]);
     // await tools.runInWorkspace("git", ["push", remoteRepo, "--tags"]);
-    // await tools.runInWorkspace("git", ["push", remoteRepo]);
+    await tools.runInWorkspace("git", ["push", remoteRepo]);
   } catch (e) {
     tools.log.fatal(e);
     tools.exit.failure("Failed to bump version");
