2b89f585e57f87804078df7ac6387a50c0882541
AswinTom Varghese <aswin.thunchathu@gmail.com>
2020-07-05 09:38:28 +0200

    refactored

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const { Toolkit } = require("actions-toolkit");
 const { execSync } = require("child_process");
 
+const getCommitMessages = (commits) => commits.map((commit) => commit.message + "\n" + commit.body);
+
+const checkIsVersionBump = (messages, commitMessage) =>
+  messages.map((message) => message.toLowerCase().includes(commitMessage)).includes(true);
+
+const setGitConfig = async (tools) => {
+  await tools.runInWorkspace("git", [
+    "config",
+    "user.name",
+    `"${process.env.GITHUB_USER || "Automated Version Bump"}"`,
+  ]);
+  await tools.runInWorkspace("git", ["config", "user.email", process.env.GITHUB_EMAIL]);
+};
+
 const getVersion = (messages) => {
   let version = "patch";
   if (messages.map((message) => message.includes("BREAKING CHANGE") || message.includes("major")).includes(true)) {
@@ -16,27 +30,41 @@ const getVersion = (messages) => {
   return version;
 };
 
-const checkIsVersionBump = (messages, commitMessage) =>
-  messages.map((message) => message.toLowerCase().includes(commitMessage)).includes(true);
+const performUpdateOnCurrentBranch = async (tools, current, version, commitMessage) => {
+  await tools.runInWorkspace("npm", ["version", "--allow-same-version=true", "--git-tag-version=false", current]);
+  console.log("current:", current, "/", "version:", version);
+  const newVersion = execSync(`npm version --git-tag-version=false ${version}`).toString().trim();
 
-const getCommitMessages = (commits) => commits.map((commit) => commit.message + "\n" + commit.body);
+  if (process.env["INPUT_SUB-PACKAGE"]) {
+    console.log("Change sub package started");
+    process.chdir(`${process.env.GITHUB_WORKSPACE}/${process.env["INPUT_SUB-PACKAGE"]}`);
+    const updatedSubVersion = execSync(`npm version --git-tag-version=false ${newVersion}`).toString().trim();
+    console.log(`Updated version of ${process.env["INPUT_SUB-PACKAGE"]}`, updatedSubVersion);
+    process.chdir(process.env.GITHUB_WORKSPACE);
+  }
 
-const setGitConfig = async (tools) => {
-  await tools.runInWorkspace("git", [
-    "config",
-    "user.name",
-    `"${process.env.GITHUB_USER || "Automated Version Bump"}"`,
-  ]);
-  await tools.runInWorkspace("git", ["config", "user.email", process.env.GITHUB_EMAIL]);
+  await tools.runInWorkspace("git", ["commit", "-a", "-m", `ci: ${commitMessage} ${newVersion}`]);
+  return newVersion;
 };
 
-// Change working directory if user defined PACKAGEJSON_DIR
-// if (process.env.PACKAGEJSON_DIR) {
-//   process.env.GITHUB_WORKSPACE = `${process.env.GITHUB_WORKSPACE}/${process.env.PACKAGEJSON_DIR}`;
-//   process.chdir(process.env.GITHUB_WORKSPACE);
-// }
+const performUpdateOnActualBranch = async (tools, current, version, currentBranch) => {
+  await tools.runInWorkspace("git", ["checkout", currentBranch]);
+  await tools.runInWorkspace("npm", ["version", "--allow-same-version=true", "--git-tag-version=false", current]);
+  console.log("current:", current, "/", "version:", version);
+  const newVersion = execSync(`npm version --git-tag-version=false ${version}`).toString().trim();
+  const tag = `${process.env["INPUT_TAG-PREFIX"]}${newVersion}`;
+  return tag;
+};
 
-// Run your GitHub Action!
+const pushChanges = async (tools, tag) => {
+  const remoteRepo = `https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`;
+  // console.log(Buffer.from(remoteRepo).toString('base64'))
+  // await tools.runInWorkspace("git", ["tag", tag]);
+  // await tools.runInWorkspace("git", ["push", remoteRepo, "--follow-tags"]);
+  // await tools.runInWorkspace("git", ["push", remoteRepo, "--tags"]);
+  await tools.runInWorkspace("git", ["push", remoteRepo]);
+};
+
 Toolkit.run(async (tools) => {
   const commitMessage = "version bump to";
 
@@ -65,34 +93,14 @@ Toolkit.run(async (tools) => {
 
     // do it in the current checked out github branch (DETACHED HEAD)
     // important for further usage of the package.json version
-    await tools.runInWorkspace("npm", ["version", "--allow-same-version=true", "--git-tag-version=false", current]);
-    console.log("current:", current, "/", "version:", version);
-    let newVersion = execSync(`npm version --git-tag-version=false ${version}`).toString().trim();
-
-    if (process.env["INPUT_SUB-PACKAGE"]) {
-      console.log("Change sub package started");
-      process.chdir(`${process.env.GITHUB_WORKSPACE}/${process.env["INPUT_SUB-PACKAGE"]}`);
-      const updatedSubVersion = execSync(`npm version --git-tag-version=false ${newVersion}`).toString().trim();
-      console.log(`{Update version ${process.env["INPUT_SUB-PACKAGE"]}}`, updatedSubVersion);
-      process.chdir(process.env.GITHUB_WORKSPACE);
-    }
-
-    await tools.runInWorkspace("git", ["commit", "-a", "-m", `ci: ${commitMessage} ${newVersion}`]);
+    let newVersion = await performUpdateOnCurrentBranch(tools, current, version, commitMessage);
 
     // now go to the actual branch to perform the same versioning
-    await tools.runInWorkspace("git", ["checkout", currentBranch]);
-    await tools.runInWorkspace("npm", ["version", "--allow-same-version=true", "--git-tag-version=false", current]);
-    console.log("current:", current, "/", "version:", version);
-    newVersion = execSync(`npm version --git-tag-version=false ${version}`).toString().trim();
-    newVersion = `${process.env["INPUT_TAG-PREFIX"]}${newVersion}`;
+    let tag = performUpdateOnActualBranch(tools, current, version, currentBranch);
 
     console.log("new version:", newVersion);
-    const remoteRepo = `https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`;
-    // console.log(Buffer.from(remoteRepo).toString('base64'))
-    // await tools.runInWorkspace("git", ["tag", newVersion]);
-    // await tools.runInWorkspace("git", ["push", remoteRepo, "--follow-tags"]);
-    // await tools.runInWorkspace("git", ["push", remoteRepo, "--tags"]);
-    await tools.runInWorkspace("git", ["push", remoteRepo]);
+
+    await pushChanges(tools, tag);
   } catch (e) {
     tools.log.fatal(e);
     tools.exit.failure("Failed to bump version");
